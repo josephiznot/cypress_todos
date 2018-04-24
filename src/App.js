4759330375f3f3af09928f80c70155666da27bd7
SheaClose <SheaClose@gmail.com>
2018-04-24 13:24:02 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import "./App.css";
 
@@ -8,6 +9,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      error: false,
+      inputText: "",
       todos: [
         // {
         //   id: 1,
@@ -16,12 +19,55 @@ class App extends Component {
         // }
       ]
     };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  handleInputChange(event) {
+    this.setState({
+      inputText: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    axios
+      .post("/api/todos", { title: this.state.inputText, isComplete: false })
+      .then(({ data }) => {
+        this.setState({
+          todos: data,
+          inputText: ""
+        });
+      })
+      .catch(() => {
+        this.setState({ error: true });
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header" />
-        <input className="new_todo" placeholder="Add new Todo" />
+        <span
+          style={{
+            display: this.state.error ? "block" : "none",
+            backgroundColor: "red",
+            height: "50px",
+            width: "200px",
+            margin: "auto"
+          }}
+        >
+          Oh No!
+        </span>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            onChange={this.handleInputChange}
+            value={this.state.inputText}
+            autoFocus
+            className="new_todo"
+            placeholder="Add new Todo"
+          />
+        </form>
         <List todos={this.state.todos} />
       </div>
     );
