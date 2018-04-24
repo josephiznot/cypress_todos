1771504a4f1c11ed812d924a4701b03ede7f25c3
SheaClose <SheaClose@gmail.com>
2018-04-24 17:11:59 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import "./App.css";
 
@@ -8,6 +9,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      error: false,
+      inputText: "",
       todos: [
         // {
         //   id: 1,
@@ -16,12 +19,61 @@ class App extends Component {
         // }
       ]
     };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleTodoSubmit = this.handleTodoSubmit.bind(this);
   }
+
+  componentDidMount() {
+    axios
+      .get("/api/todos")
+      .then(({ data }) => {
+        this.setState({
+          todos: data
+        });
+      })
+      .catch(() => {
+        this.setState({
+          error: true
+        });
+      });
+  }
+
+  handleInputChange(e) {
+    this.setState({
+      inputText: e.target.value
+    });
+  }
+
+  handleTodoSubmit(e) {
+    e.preventDefault();
+    axios
+      .post("/api/todos", { title: this.state.inputText, isComplete: true })
+      .then(({ data }) => {
+        this.setState({ todos: data, inputText: "" });
+      })
+      .catch(() => {
+        this.setState({
+          error: true
+        });
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header" />
-        <input className="new_todo" placeholder="Add new Todo" />
+        {this.state.error && (
+          <span className="error">Oh No! Something Broke!</span>
+        )}
+        <form onSubmit={this.handleTodoSubmit}>
+          <input
+            onChange={this.handleInputChange}
+            autoFocus
+            value={this.state.inputText}
+            className="new_todo"
+            placeholder="Add new Todo"
+          />
+        </form>
         <List todos={this.state.todos} />
       </div>
     );
