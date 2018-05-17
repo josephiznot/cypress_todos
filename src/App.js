cd49b0888c0c60c75f65abff6db068484814574a
Joseph Anderson <josephiznot@gmail.com>
2018-05-17 10:29:32 -0500

    focused test

--- src/App.js
+++ src/App.js
@@ -14,14 +14,20 @@ class App extends Component {
         //   title: "Teach Cypress Testing Suite",
         //   isComplete: false
         // }
-      ]
+      ],
+      inputText: "this is a default value"
     };
   }
   render() {
     return (
       <div className="App">
         <header className="App-header" />
-        <input className="new_todo" placeholder="Add new Todo" />
+        <input
+          value={this.state.inputText}
+          autoFocus
+          className="new_todo"
+          placeholder="Add new Todo"
+        />
         <List todos={this.state.todos} />
       </div>
     );
